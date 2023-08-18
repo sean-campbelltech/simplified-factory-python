d916f1c659bceb471dc0547f8563ae77b2f4c99d
sean <sean@campbelltech.co.za>
2023-08-18 16:21:41 +0200

    Revert to global variable

--- payment_factory.py
+++ payment_factory.py
@@ -4,8 +4,9 @@ import payment_methods
 
 
 class PaymentFactory(object):
+    payment_implementations = {}
+
     def __init__(self):
-        self._payment_implementations = {}
         self.load_payment_methods()
 
     def load_payment_methods(self):
@@ -14,11 +15,11 @@ class PaymentFactory(object):
         )
         for name, _type in implementations:
             if isclass(_type) and issubclass(_type, Payment):
-                self._payment_implementations[name] = _type
+                self.payment_implementations[name] = _type
 
     def create(self, payment_type: str):
-        if payment_type in self._payment_implementations:
-            return self._payment_implementations[payment_type]()
+        if payment_type in self.payment_implementations:
+            return self.payment_implementations[payment_type]()
         else:
             raise ValueError(
                 f"{payment_type} is not currently supported as a payment method."
