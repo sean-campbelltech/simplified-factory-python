cb24d1274df9ed22888a60cbfb2ee2394f0cdce0
sean <sean@campbelltech.co.za>
2023-08-18 16:15:43 +0200

    Changed to class instance variable and added _ to indicate that it should be treated as private

--- payment_factory.py
+++ payment_factory.py
@@ -4,9 +4,8 @@ import payment_methods
 
 
 class PaymentFactory(object):
-    payment_implementations = {}
-
     def __init__(self):
+        self._payment_implementations = {}
         self.load_payment_methods()
 
     def load_payment_methods(self):
@@ -15,11 +14,11 @@ class PaymentFactory(object):
         )
         for name, _type in implementations:
             if isclass(_type) and issubclass(_type, Payment):
-                self.payment_implementations[name] = _type
+                self._payment_implementations[name] = _type
 
     def create(self, payment_type: str):
-        if payment_type in self.payment_implementations:
-            return self.payment_implementations[payment_type]()
+        if payment_type in self._payment_implementations:
+            return self._payment_implementations[payment_type]()
         else:
             raise ValueError(
                 f"{payment_type} is not currently supported as a payment method."
